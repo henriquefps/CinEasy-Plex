85728a8908768ce5ff441fa8a655f4d01b6392f8
Henrique Farias <henriquefps@hotmail.com>
2018-01-16 10:25:41 -0300

    Complemento do Commit Anterior

--- CinEasy Plex/src/controladores/TelaConfiguracoesController.java
+++ CinEasy Plex/src/controladores/TelaConfiguracoesController.java
@@ -1,5 +1,59 @@
 package controladores;
 
+import javafx.fxml.FXML;
+
 public class TelaConfiguracoesController {
 
+	@FXML public void voltarParaMenuPrincipal(){
+		// TODO
+	}
+	
+	@FXML public void abrirAdicionarContas(){
+		// TODO
+	}
+	@FXML public void abrirAdicionarFilmes(){
+		// TODO
+	}
+	@FXML public void abrirAdicionarSalas(){
+		// TODO
+	}
+	@FXML public void abrirAdicionarSessoes(){
+		// TODO
+	}
+	@FXML public void abrirRemoverContas(){
+		// TODO
+	}
+	@FXML public void abrirRemoverFilmes(){
+		// TODO
+	}
+	@FXML public void abrirRemoverSalas(){
+		// TODO	
+	}
+	@FXML public void abrirRemoverSessoes(){
+		// TODO
+	}
+	@FXML public void abrirAlterarContas(){
+		// TODO
+	}
+	@FXML public void abrirAlterarFilmes(){
+		// TODO
+	}
+	@FXML public void abrirAlterarSalas(){
+		// TODO
+	}
+	@FXML public void abrirAlterarSessoes(){
+		// TODO
+	}
+	@FXML public void abrirListarContas(){
+		// TODO
+	}
+	@FXML public void abrirListarFilmes(){
+		// TODO
+	}
+	@FXML public void abrirListarSalas(){
+		// TODO
+	}
+	@FXML public void abrirListarSessoes(){
+		// TODO
+	}
 }
--- CinEasy Plex/src/controladores/Tela_MenuPrincipalController.java
+++ CinEasy Plex/src/controladores/Tela_MenuPrincipalController.java
@@ -6,14 +6,14 @@ public class Tela_MenuPrincipalController {
 	
 	
 	@FXML public void sairParaLogin(){
-		
+		//TODO abrir Tela Login
 	}
 	
 	@FXML public void abrirVenderIngressos(){
-		
+		// TODO abrir tela Vender Ingressos
 	}
 	
 	@FXML public void abrirConfiguracoes(){
-		
+		// TODO abrir tela Configuracoes
 	}
 }
