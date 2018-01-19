700a7948f7d342980ba5edbaa62298f9ada1c690
Pedro <Pedro@192.168.1.10>
2018-01-19 17:56:43 -0300

    tela listar sessão

--- CinEasy Plex/src/controladores/Tela_ListarSessaoController.java
+++ CinEasy Plex/src/controladores/Tela_ListarSessaoController.java
@@ -3,43 +3,62 @@ package controladores;
 import java.beans.PropertyDescriptor;
 import java.net.URL;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.LocalTime;import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
 import java.util.ListIterator;
 import java.util.ResourceBundle;
 
-import com.sun.javafx.property.PropertyReference;
+import com.sun.javafx.scene.control.skin.TextFieldSkin;
 
-import beans.Filme;
 import beans.Sessao;
 import fachada.CinemaFachada;
-import javafx.beans.InvalidationListener;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
-import javafx.collections.ListChangeListener;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TableColumn.CellDataFeatures;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.util.Callback;
 
 public class Tela_ListarSessaoController implements Initializable {
 
 	@FXML private TableView<Sessao> tvSessao;
 	@FXML private TableColumn<Sessao, Integer> id;
 	@FXML private TableColumn<Sessao, String> filme;
-	@FXML private TableColumn<Sessao, LocalDate> data;
-	@FXML private TableColumn<Sessao, LocalTime> duracao;
-	@FXML private TableColumn<Sessao, LocalTime> horaInicio;
-	@FXML private TableColumn<Sessao, LocalTime> horaFim;
-	@FXML private TableColumn<Sessao, Integer> sala;
+	@FXML private TableColumn<Sessao, LocalDateTime> data;
+	@FXML private TableColumn<Sessao, String> duracao;
+	@FXML private TableColumn<Sessao, LocalDateTime> horaFim;
+	@FXML private TableColumn<Sessao, String> sala;
+	@FXML private TextField tfIdSessao, tfFilme, tfIdSala;
+	
 	private CinemaFachada f;
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		// TODO Auto-generated method stub
 		f = CinemaFachada.getInstance();
 		preencherTabela();
+	}
+	
+	@FXML 
+	public void pesquisarSessao(){
+		
+	}
+	
+	@FXML
+	public void pesquisarFilme(){
+		
+	}
+	
+	@FXML
+	public void pesquisarSala(){
+		
 	}
 	
 	@FXML
@@ -49,10 +68,31 @@ public class Tela_ListarSessaoController implements Initializable {
 	
 	public void preencherTabela(){
 		id.setCellValueFactory(new PropertyValueFactory<>("idSessao"));
-		filme.setCellValueFactory(new PropertyValueFactory<>("titulo"));
+		filme.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<Sessao,String>, ObservableValue<String>>() {
+			@Override
+			public ObservableValue<String> call(CellDataFeatures<Sessao, String> param) {
+				// TODO Auto-generated method stub
+				return new SimpleStringProperty(param.getValue().getFilmeExibido().getTitulo());
+			}
+		});
+		data.setCellValueFactory(new PropertyValueFactory<>("inicioDaSessao"));
+		horaFim.setCellValueFactory(new PropertyValueFactory<>("fimDaSessao"));
+		duracao.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<Sessao,String>, ObservableValue<String>>() {
+
+			@Override
+			public ObservableValue<String> call(CellDataFeatures<Sessao, String> arg0) {
+				// TODO Auto-generated method stub
+				return new SimpleStringProperty(arg0.getValue().getFilmeExibido().getDuracao().toString());
+			}
+		});
+		sala.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<Sessao,String>, ObservableValue<String>>() {
+			
+			@Override
+			public ObservableValue<String> call(CellDataFeatures<Sessao, String> arg0) {
+				// TODO Auto-generated method stub
+				return new SimpleStringProperty(arg0.getValue().getSalaDeExibicao().getTipo().toString());
+			}
+		});
 		tvSessao.setItems(FXCollections.observableArrayList(f.listarTodasSessao()));
 	}
-	
-	
-
 }
