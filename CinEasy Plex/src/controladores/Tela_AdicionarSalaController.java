ad960a86b270c0be382fbe3589a58798d80eeeaf
Henrique Farias <henriquefps@hotmail.com>
2018-01-22 17:45:28 -0300

    Tela Alterar Sala e (Remover Sala)* Prontas
    * Ainda precisa Removesser todas as sessões relacionadas a uma sala quando remove uma sala.

--- CinEasy Plex/src/controladores/Tela_AdicionarSalaController.java
+++ CinEasy Plex/src/controladores/Tela_AdicionarSalaController.java
@@ -17,64 +17,64 @@ import javafx.scene.control.TextField;
 import javafx.scene.paint.Color;
 
 public class Tela_AdicionarSalaController {
-	
+
 	Sala a = new Sala();
 	ObservableList<TipoSala> tiposSalaS = FXCollections.observableArrayList(TipoSala.values());
-	
-	@FXML TextField id;
-	@FXML ChoiceBox tipoSala;
-	@FXML Spinner linhas;
-	@FXML Spinner colunas;
-	@FXML CheckBox d3;
-	@FXML Label texto;
 
-	@FXML private void initialize()
-	{	
+	@FXML
+	TextField id;
+	@FXML
+	ChoiceBox<TipoSala> tipoSala;
+	@FXML
+	Spinner<Integer> linhas;
+	@FXML
+	Spinner<Integer> colunas;
+	@FXML
+	CheckBox d3;
+	@FXML
+	Label texto;
+
+	@FXML
+	private void initialize() {
 		tipoSala.setItems(tiposSalaS);
-		
+
 		SpinnerValueFactory<Integer> valor = new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 20);
 		SpinnerValueFactory<Integer> valor1 = new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 20);
 		this.linhas.setValueFactory(valor);
 		this.colunas.setValueFactory(valor1);
-		
+
 	}
-	
+
 	@FXML
 	public void voltarParaConfiguracoes() {
 		ScreenManager.setScene(ScreenManager.getInstance().getTelaConfiguracao());
 	}
 
-	
 	@FXML
 	public void cadastrarSala() {
 
-	try {
-	a = new Sala((int) linhas.getValue(), (int) colunas.getValue(), (TipoSala) tipoSala.getValue(), d3.isSelected());
-	System.out.println(a.toString());
-	CinemaFachada.getInstance().cadastrarSala(a);
-	
+		try {
+			a = new Sala((int) linhas.getValue(), (int) colunas.getValue(), (TipoSala) tipoSala.getValue(),
+					d3.isSelected());
+			System.out.println(a.toString());
+			CinemaFachada.getInstance().cadastrarSala(a);
+
+		} catch (Exception e) {
+
+			texto.setTextFill(Color.RED);
+			texto.setText("Um erro ocorreu!");
+
+		}
+
+		tipoSala.setValue(null);
+		linhas.getValueFactory().setValue(0);
+
+		colunas.getValueFactory().setValue(0);
+
+		texto.setTextFill(Color.GREEN);
+		texto.setText("Sala cadastrada");
+
+		d3.setSelected(false);
 	}
-	catch(Exception e){
-		
-		texto.setTextFill(Color.RED);
-		texto.setText("Um erro ocorreu!");
-		
-	}
-
-	tipoSala.setValue(null);
-	linhas.getValueFactory().setValue(0);
-
-	colunas.getValueFactory().setValue(0);
-	
-	texto.setTextFill(Color.GREEN);
-	texto.setText("Sala cadastrada");
-	
-	d3.setSelected(false);
-	}
-
-	
-	
-
-
 
 }
---
+++ CinEasy Plex/src/controladores/Tela_RemoverSalaController.java
@@ -0,0 +1,58 @@
+package controladores;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+import beans.Sala;
+import fachada.CinemaFachada;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TableColumn.CellDataFeatures;
+import javafx.util.Callback;
+
+public class Tela_RemoverSalaController implements Initializable{
+	@FXML private TableView<Sala> tableViewSalas;
+	@FXML private TableColumn<Sala, String> tableColumnIdSala;
+	
+	private Sala selecionada = null;
+	
+	@FXML public void selecionarSala(){
+		selecionada = tableViewSalas.getSelectionModel().getSelectedItem();
+	}
+	
+	@FXML public void removerSala(){
+		if(selecionada != null){
+			try {
+				//TODO LANCAR ALERTA
+				CinemaFachada.getInstance().removerSala(selecionada);
+			} catch (Exception e) {e.printStackTrace();}
+		}
+	}
+	
+	@FXML public void voltarParaConfiguracoes(){
+		ScreenManager.setScene(ScreenManager.getInstance().getTelaConfiguracao());
+	}
+	
+	private void preencherTabela(){
+		ArrayList<Sala> listaDeSalas = CinemaFachada.getInstance().listarTodasSala();
+		tableColumnIdSala.setCellValueFactory(new Callback<CellDataFeatures<Sala,String>, ObservableValue<String>>() {
+			@Override
+			public ObservableValue<String> call(CellDataFeatures<Sala, String> todosAsSalas) {
+				return new SimpleStringProperty(Byte.valueOf(todosAsSalas.getValue().getIdSala()).toString());
+				}
+			});
+		tableViewSalas.setItems(FXCollections.observableArrayList(listaDeSalas));
+		tableViewSalas.refresh();
+	}
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		preencherTabela();
+	}
+}
