836fe6b935553e4d439ba0b795bdca629dce51bd
Ariany Ferreira <fumigari@users.noreply.github.com>
2018-01-23 23:40:12 -0300

    Commit Conta

--- CinEasy Plex/src/repositorios/RepositorioContasArray.java
+++ CinEasy Plex/src/repositorios/RepositorioContasArray.java
@@ -1,13 +1,23 @@
 package repositorios;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import com.mysql.jdbc.PreparedStatement;
+import com.sun.xml.internal.ws.client.sei.ValueSetter;
+
 import java.util.ArrayList;
 
 import beans.Conta;
+
 import interfaces.IRepositorioContas;
 
-public class RepositorioContasArray implements IRepositorioContas {
+public class RepositorioContasArray implements InterfaceCRUD<Conta, String> {
 
 	private static RepositorioContasArray instance;
+	private PreparedStatement statement;
+	private ResultSet rSet;
 	private ArrayList<Conta> repositorio;
 	private static int id;
 	
@@ -26,29 +36,73 @@ public class RepositorioContasArray implements IRepositorioContas {
 
 
 	@Override
-	public void cadastrar(Conta obj) {
+	public boolean existe(String login) throws SQLException{
+		boolean resultado = false;
+		String sql = "SELECT * FROM conta WHERE  login=" + login;
+		
+		this.statement = (PreparedStatement) ConnectionFactory.getInstance().getConnection().prepareStatement(sql);
+		this.rSet = (ResultSet) statement.executeQuery();
+		
+		if(rSet.next()) {
+			resultado = true;
+		}
+		
+		return resultado;
+		
+	}
+	
+	@Override
+	public boolean cadastrar(Conta obj) throws SQLException {
 		id += 1;
 		obj.setIdConta(id);
-		repositorio.add(obj);
+		String sql = "INSERT INTO conta(idConta, SENHA)" + "values(?,?)";
+		
+		statement = (PreparedStatement) ConnectionFactory.getInstance().getConnection().prepareStatement(sql);
+		statement.setLong(1, obj.getIdConta());
+		statement.setString(2, obj.getLogin());
+		statement.setString(3, obj.getSenha());
+		statement.execute();
+		
+		return true;
 	}
 
 
 	@Override
-	public void atualizar(Conta newObj) {
-		repositorio.set(newObj.getIdConta()-1, newObj);
+	public boolean atualizar(Conta obj) throws SQLException {
+		String sql = "UPDATE cinEasy_Plex.Conta SET login = ?, senha = ?, idConta = ?"
+				+ "WHERE idConta =" + obj.getIdConta();
+		
+		statement = (PreparedStatement) ConnectionFactory.getInstance().getConnection().prepareStatement(sql);
+		statement.setString(1, obj.getLogin());
+		statement.setString(2, obj.getSenha());
+		statement.execute();
+		
+		return true;	
+				
+//		repositorio.set(newObj.getIdConta()-1, newObj);
 		
 	}
 
 
 	@Override
-	public void remover(Conta obj) {
-		repositorio.remove(obj);
+	public boolean remover(Conta obj) throws SQLException{
+		String sql = "DELETE FROM cinEasy_Plex.Conta" + "WHERE login = ?";
 		
+		statement = (PreparedStatement) ConnectionFactory.getInstance().getConnection().prepareStatement(sql);
+		statement.setString(1, obj.getLogin());
+		statement.execute();
+		
+		return true;
 	}
 
 
 	@Override
-	public Conta buscar(int id) {
+	public Conta buscar(String login) throws SQLException{
+		Conta conta = null;
+		String sql = "SELECT * FROM conta WHERE login =" + login;
+		
+		this.statement = (PreparedStatement) ConnectionFactory.getInstance().getConnection().prepareStatement(sql);
+		this.rSet = (ResultSet) statement.executeQuery();
 		return repositorio.get(id-1);
 	}
 
